a3118f39f2731e9b325a68b623ca60eeac3ef4b0
loyal <102851403+loyaltly-cn@users.noreply.github.com>
2025-09-30 13:28:50 +0800

    update packge.json

--- src/api/user/login.ts
+++
@@ -1,8 +0,0 @@
-import type {Hono} from "hono";
-
-export default (app: Hono) => {
-    app.post("/login", async (c) => {
-        const body = await c.req.json()
-        return c.json({ message: `Welcome ${body.username}` })
-    })
-}
